9beec17a010a69cdcb14aa3359c50071be3a6b0c
Tucker McKee <tuckermckee@Tuckers-MacBook-Pro.local>
2024-07-01 22:02:15 -0500

    init commit

---
+++ LetandConst.js
@@ -0,0 +1,6 @@
+const pi = 3.14;
+
+// let does not allow for redeclaration and is block scope, var is function scope
+// const cannot be reassigned or redeclared and is block scope
+// let can be reassigned
+// when a variable is lifted to the top of the scope it is declared in
